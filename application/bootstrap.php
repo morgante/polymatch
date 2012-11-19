60d158c685a8c64348fcb0eab11e0d8e115bdf40
Morgante Pell <morgante.pell@morgante.net>
2012-11-19 15:25:14 +0400

    test

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -123,6 +123,8 @@ Kohana::modules(array(
 	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
 	));
 
+print_r( Kohana::find_file( 'classes/controller', 'test') );
+
 /**
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
