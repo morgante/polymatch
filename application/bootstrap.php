b05b2de46fbf525eaf60266478f789ed4cb51c50
Morgante Pell <morgante.pell@morgante.net>
2012-11-20 20:27:11 +0400

    testing

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -7,7 +7,7 @@ require SYSPATH.'classes/Kohana/Core'.EXT;
 
 print_r( APPPATH.'classes/Kohana'.EXT );
 
-print_r( is_file(APPPATH.'classes/Kohana'.EXT) );
+print_r( scandir( APPPATH ) );
 
 if (is_file(APPPATH.'classes/Kohana'.EXT))
 {
