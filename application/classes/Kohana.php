86fc1b8b2c4a30bff8c4acf5cfa3a7a3a2890210
Morgante Pell <morgante.pell@morgante.net>
2012-11-21 19:57:44 +0400

    test fix

--- application/classes/Kohana.php
+++ application/classes/Kohana.php
@@ -19,12 +19,6 @@ class Kohana extends Kohana_Core {
 		}
 
 		$file .= str_replace('_', DIRECTORY_SEPARATOR, $class);
-		
-		if( $class == 'Database_Mysql' )
-		{
-			print_r( '<br>' . Kohana::find_file($directory, $file) );
-			exit;
-		}
 				
 		if ($path = Kohana::find_file($directory, $file))
 		{
@@ -37,6 +31,97 @@ class Kohana extends Kohana_Core {
 
 		// Class is not in the filesystem
 		return FALSE;
+	}
+	
+	public static function find_file($dir, $file, $ext = NULL, $array = FALSE)
+	{
+		if( $file == 'Database/Mysql' )
+		{
+			print_r( $file . '<br>');
+		}
+		
+		if ($ext === NULL)
+		{
+			// Use the default extension
+			$ext = EXT;
+		}
+		elseif ($ext)
+		{
+			// Prefix the extension with a period
+			$ext = ".{$ext}";
+		}
+		else
+		{
+			// Use no extension
+			$ext = '';
+		}
+
+		// Create a partial path of the filename
+		$path = $dir.DIRECTORY_SEPARATOR.$file.$ext;
+
+		if (Kohana::$caching === TRUE AND isset(Kohana::$_files[$path.($array ? '_array' : '_path')]))
+		{
+			// This path has been cached
+			return Kohana::$_files[$path.($array ? '_array' : '_path')];
+		}
+
+		if (Kohana::$profiling === TRUE AND class_exists('Profiler', FALSE))
+		{
+			// Start a new benchmark
+			$benchmark = Profiler::start('Kohana', __FUNCTION__);
+		}
+
+		if ($array OR $dir === 'config' OR $dir === 'i18n' OR $dir === 'messages')
+		{
+			// Include paths must be searched in reverse
+			$paths = array_reverse(Kohana::$_paths);
+
+			// Array of files that have been found
+			$found = array();
+
+			foreach ($paths as $dir)
+			{
+				if (is_file($dir.$path))
+				{
+					// This path has a file, add it to the list
+					$found[] = $dir.$path;
+				}
+			}
+		}
+		else
+		{
+			// The file has not been found yet
+			$found = FALSE;
+
+			foreach (Kohana::$_paths as $dir)
+			{
+				if (is_file($dir.$path))
+				{
+					// A path has been found
+					$found = $dir.$path;
+
+					// Stop searching
+					break;
+				}
+			}
+		}
+
+		if (Kohana::$caching === TRUE)
+		{
+			// Add the path to the cache
+			Kohana::$_files[$path.($array ? '_array' : '_path')] = $found;
+
+			// Files have been changed
+			Kohana::$_files_changed = TRUE;
+		}
+
+		if (isset($benchmark))
+		{
+			// Stop the benchmark
+			Profiler::stop($benchmark);
+		}
+
+		return $found;
 	}
 		
 }
